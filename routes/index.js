0d76c505ed63454d12efff427b48716f17c45ace
atharvyadav96k <atharvyadav96k@gmail.com>
2024-05-01 01:23:21 +0530

    created room schema and first demo room

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+const roomSchame = require('../schema/roomSchema');
+const encryptor = require('simple-encryptor')("mother Father Father Mother");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.post('/createRoom', async function(req, res){
+  const {password, hr} = req.body;
+  const newRoom = new roomSchame({
+    name: '1234',
+    password: encryptor.encrypt(password),
+    hr: hr
+  });
+  await newRoom.save();
+  res.send(password+hr);
 });
 
 module.exports = router;
---
+++ schema/roomSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost:27017/onlinetextshare");
+
+
+const roomShema = mongoose.Schema({
+    name: String,
+    password: String,
+    hr: Number
+});
+
+module.exports = mongoose.model("/room", roomShema);
